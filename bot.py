033684e132f9f6cc39ac9193bf258a160cab6d5f
Artiya Thinkumpang <artiya4u@gmail.com>
2020-03-25 13:16:34 +0700

    Add removing change group title message.

--- bot.py
+++ bot.py
@@ -4,9 +4,15 @@ app = Client("silencert")
 
 
 @app.on_message(Filters.group)
-def delete_join_or_leave_message(client, message):
-    if message.new_chat_members is not None or message.left_chat_member is not None:
-        client.delete_messages(message.chat.id, [message.message_id])
+def delete_unwanted_message(client, message):
+    unwanted_messages = [
+        message.new_chat_members,
+        message.left_chat_member,
+        message.new_chat_title,
+    ]
+    for unwanted in unwanted_messages:
+        if unwanted is not None:
+            client.delete_messages(message.chat.id, [message.message_id])
 
 
 app.start()

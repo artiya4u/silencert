c59f08ee27abe4580adce62ffa28a6a58c63a68a
Artiya Thinkumpang <artiya4u@gmail.com>
2020-10-22 13:42:42 +0700

    Add leaving scam group.

--- leave_group.py
+++ leave_group.py
@@ -7,12 +7,15 @@ dialogs = app.iter_dialogs()
 me = app.get_me()
 for d in dialogs:
     try:
-        if d.chat.members_count < 20:
+        member = app.get_chat_member(d.chat.id, me.id)
+        if d.chat.is_scam:
+            print("LEAVE - SCAM", d.chat.title)
+            app.leave_chat(d.chat.id)
+        elif d.chat.members_count < 20:
             print("LEAVE - NOT ENOUGH USERS", d.chat.title)
             app.leave_chat(d.chat.id)
-        member = app.get_chat_member(d.chat.id, me.id)
-        if member.status != "administrator":
-            print("LEAVE", d.chat.title)
+        elif member.status != "administrator":
+            print("LEAVE - NOT ADMIN", d.chat.title)
             app.leave_chat(d.chat.id)
         else:
             print('STAY', d.chat.title)
